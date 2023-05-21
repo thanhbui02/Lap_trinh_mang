a89f0bb3ab5584e518156b6be9b9ff489ac269cd
thanhbui02 <thanh.bt204851@sis.hust.edu.vn>
2023-05-21 20:22:20 +0700

    Bài tập ngày 08/05/2023

--- baitap_08052023/chat/chat_server.c
+++ baitap_08052023/chat/chat_server.c
@@ -6,7 +6,9 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <sys/select.h>
+#include <poll.h> // Include poll.h for using the poll function
+
+#define MAX_CLIENTS 64
 
 int main()
 {
@@ -34,48 +36,36 @@ int main()
         return 1;
     }
 
-    fd_set fdread;
+    struct pollfd fds[MAX_CLIENTS + 1]; // Define the pollfd array (+1 for the listener)
+    int nfds = 1;                       // Number of file descriptors, starting from 1 with the listener
 
-    int clients[64];
-    char nameClients[64][50];
+    fds[0].fd = listener;
+    fds[0].events = POLLIN; // Event for read (data available)
+
+    int clients[MAX_CLIENTS];
+    char nameClients[MAX_CLIENTS][50];
     int num_clients = 0;
 
     char buf[256];
 
     while (1)
     {
-        // Xóa tất cả socket trong tập fdread
-        FD_ZERO(&fdread);
-
-        // Thêm socket listener vào tập fdread
-        FD_SET(listener, &fdread);
-        int maxdp = listener + 1;
-
-        // Thêm các socket client vào tập fdread
-        for (int i = 0; i < num_clients; i++)
-        {
-            FD_SET(clients[i], &fdread);
-            if (maxdp < clients[i] + 1)
-                maxdp = clients[i] + 1;
-        }
-
-        // Chờ đến khi sự kiện xảy ra
-        int ret = select(maxdp, &fdread, NULL, NULL, NULL);
+        int ret = poll(fds, nfds, -1); // -1 to wait indefinitely
 
         if (ret < 0)
         {
-            perror("select() failed");
+            perror("poll() failed");
             return 1;
         }
 
-        // Kiểm tra sự kiện có yêu cầu kết nối
-        if (FD_ISSET(listener, &fdread))
+        // Check for connection request event
+        if (fds[0].revents & POLLIN)
         {
             char buff[50];
             char id[20];
             char name[20];
-            int count=0;
-            char space=' ';
+            int count = 0;
+            char space = ' ';
             memset(buff, 0, sizeof(buff));
             memset(id, 0, sizeof(id));
             memset(name, 0, sizeof(name));
@@ -83,9 +73,9 @@ int main()
             printf("Có kết nối mới: %d\n", client);
             do
             {
-                count=0;
-                memset(name,'\0',sizeof(name));
-                memset(buff,'\0',sizeof(buff));
+                count = 0;
+                memset(name, '\0', sizeof(name));
+                memset(buff, '\0', sizeof(buff));
                 int s = send(client, "Vui lòng nhập tên của bạn (đúng định dạng client_id: name):", strlen("Vui lòng nhập tên của bạn (đúng định dạng client_id:name):"), 0);
                 if (s <= 0)
                 {
@@ -107,40 +97,62 @@ int main()
                 strcpy(name, token);
                 for (int i = 1; i < strlen(name); i++)
                 {
-                    if (name[i] ==space)
+                    if (name[i] == space)
                     {
                         count++;
                     }
                 }
-            } while (strcmp(id, "client_id") != 0 || strlen(name) < 3 || count >=1);
-            clients[num_clients] = client;
-            strcpy(nameClients[num_clients++], name);
-            printf("%s đã kết nối tới Server chat.\n", nameClients[num_clients - 1]);
+            } while (strcmp(id, "client_id") != 0 || strlen(name) < 3 || count >= 1);
+
+            if (num_clients >= MAX_CLIENTS)
+            {
+                printf("Số lượng kết nối đã đạt tối đa. Từ chối kết nối mới.\n");
+                close(client);
+            }
+            else
+            {
+                clients[num_clients] = client;
+                strcpy(nameClients[num_clients], name);
+                printf("%s đã kết nối tới Server chat.\n", nameClients[num_clients]);
+                fds[nfds].fd = client;
+                fds[nfds].events = POLLIN;
+                nfds++;
+                num_clients++;
+            }
         }
 
-        // Kiểm tra sự kiện có dữ liệu truyền đến socket client
-        for (int i = 0; i < num_clients; i++)
-            if (FD_ISSET(clients[i], &fdread))
+        // Check for data received event from client sockets
+        for (int i = 1; i < nfds; i++)
+        {
+            if (fds[i].revents & POLLIN)
             {
-                ret = recv(clients[i], buf, sizeof(buf), 0);
+                ret = recv(fds[i].fd, buf, sizeof(buf), 0);
                 if (ret <= 0)
                 {
-                    // TODO: Client đã ngắt kết nối, xóa client ra khỏi mảng
+                    // Client đã ngắt kết nối, xóa client ra khỏi mảng và giảm số lượng nfds
+                    printf("%s đã ngắt kết nối.\n", nameClients[i - 1]);
+                    close(fds[i].fd);
+                    for (int j = i; j < nfds - 1; j++)
+                    {
+                        fds[j] = fds[j + 1];
+                    }
+                    nfds--;
+                    i--; // Giảm chỉ số i để kiểm tra socket mới đến
                     continue;
                 }
                 buf[ret] = 0;
-                // printf(" %s : %s\n", nameClients[i], buf);
                 char full_msg[500];
-                int msg_len = snprintf(full_msg, sizeof(full_msg), "%s:%s", nameClients[i], buf);
+                int msg_len = snprintf(full_msg, sizeof(full_msg), "%s:%s", nameClients[i - 1], buf);
                 full_msg[strlen(full_msg) - 1] = 0;
-                for (int j = 0; j < num_clients; j++)
+                for (int j = 1; j < nfds; j++)
                 {
                     if (j != i)
                     {
-                        send(clients[j], full_msg, strlen(full_msg), 0);
+                        send(fds[j].fd, full_msg, strlen(full_msg), 0);
                     }
                 }
             }
+        }
     }
 
     close(listener);
